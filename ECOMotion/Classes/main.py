38b82ceabb29dc40e3e685d2a1b73b3f1f5a7f20
Raiffer <144740036+Raiffer@users.noreply.github.com>
2024-11-24 06:43:01 -0300

    Add files via upload

--- ECOMotion/Classes/main.py
+++ ECOMotion/Classes/main.py
@@ -40,30 +40,31 @@ def individual():
     veiculo1 = Veiculo("Volkswagen Delivery 11.180", 7500, "PMO-7008", 5.4, 10800)
     veiculo2 = Veiculo("Volkswagen Delivery 11.180", 7500, "CPX-2315", 5.4, 10800)
 
-    frete1 = Frete("Juazeiro-do-Norte", "Russas", 364.8, veiculo1, 3200, "CMOtion","Mobilia", gas)
-    frete2 = Frete("Juazeiro-do-Norte", "Russas", 364.8, veiculo2, 3000, "Empresa A","Mobilia", gas)
+    frete1 = Frete("Juazeiro-do-Norte", "Russas", 364.8, veiculo1, "CMOtion","Mobilia", gas)
+    frete2 = Frete("Juazeiro-do-Norte", "Russas", 364.8, veiculo2, "Empresa A","Mobilia", gas)
  
     pegada = frete1.pegada_co2(emissao_disel)+frete2.pegada_co2(emissao_disel)
 
     print("-------------------------CASO INDIVIDUAL-------------------------")
-    print("CMOtion:")
+    print("Empresa A:")
     print(frete1)
     frete1.detalhes()
-    frete1.economia()
+    frete1.adicionar_carga("Empresa A", 3040,"Mobilia")
     print(f"Durante essa viagem, foram liberados {frete1.pegada_co2(emissao_disel):.2f} kg de CO2 na Atmosfera")
     print()
 
-    print("Empresa A:")
+    print("Empresa B:")
     print(frete2)
     frete2.detalhes()
-    frete2.economia()
+    frete2.adicionar_carga("Empresa B", 3040,"Mobilia")
     print(f"Durante essa viagem, foram liberados {frete2.pegada_co2(emissao_disel):.2f} kg de CO2 na Atmosfera")
     print()
 
+    pegada = frete2.pegada_co2(emissao_disel) + frete1.pegada_co2(emissao_disel)
     print(f"Ao todo, as tres empresas liberaram {pegada:.2f} kg de CO2 na Atmosfera")
     return pegada
 
 comp = compartilhado()
-#ind = individual()
+ind = individual()
 
-#print(f"Em comparação, a nossa solução reduziu em {((ind-comp)/ind)*100:.2f}% a emissão de CO2 na Atmosfera")
+print(f"Em comparação, a nossa solução reduziu em {((ind-comp)/ind)*100:.2f}% a emissão de CO2 na Atmosfera")
